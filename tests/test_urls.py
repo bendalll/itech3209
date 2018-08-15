16a6e05ddfd6ebf247de9e007975c7c862740806
bendalll <42238838+bendalll@users.noreply.github.com>
2018-08-15 17:57:43 +1000

    Working on adding tests and coverage

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -1,5 +1,10 @@
 from django.urls import resolve
+from django.test import TestCase
+from MyDigitalHealth.views import cards
 
-# 
-resolver = resolve('/cards/')
-assert(resolver.func.func_name, 'cards')
+
+class ViewTest(TestCase):
+
+    def test_cards_url_resolves_to_cards_view(self):
+        found = resolve('/MyDigitalHealth/cards')
+        self.assertEqual(found.func, cards)
---
+++ tests/test_views.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+
+
+class ViewsTest(TestCase):
+
+    def test_index_view(self):
+        assert True
