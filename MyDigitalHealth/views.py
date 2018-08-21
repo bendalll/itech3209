1ad13cbe917c0e0a08360956a316817f4759de1f
bendalll <42238838+bendalll@users.noreply.github.com>
2018-08-21 16:58:32 +1000

    Formatting update

--- MyDigitalHealth/views.py
+++ MyDigitalHealth/views.py
@@ -7,32 +7,36 @@ from .models import Card_Packages, Card_Groups, Cards
 from django.template.response import TemplateResponse
 from django.contrib import messages 
 
+
 def index(request):
     """
     View function for home page of site.
     """
-	# Number of visits to this view, as counted in the session variable.
-    num_visits=request.session.get('num_visits', 0)
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
     request.session['num_visits'] = num_visits+1
     # Render the HTML template index.html with the data in the context variable
     return render(
         request,
         'index.html',
-		context={'num_visits':num_visits}, # num_visits appended
+        context={'num_visits': num_visits},  # num_visits appended
     )
-	
+
+
 def create(request):
     return render(
         request,
         'project.html',
     )
-	
+
+
 def getCards(request):
     cards = Cards.objects.all() 
     return TemplateResponse(request, views.index, {'cards': cards})
-	
+
+
 def cards(request):
-    if request.method =='POST':
+    if request.method == 'POST':
         form = CreateCardPackage(request.POST, instance=Card_Packages())
         if form.is_valid():
             titles = request.POST.getlist('title')
@@ -40,37 +44,38 @@ def cards(request):
             groups = request.POST.getlist('group')
             names = request.POST.getlist('name')
             for name in names:
-                cardPackage = Card_Packages(name = name)
+                cardPackage = Card_Packages(name=name)
                 cardPackage.save()
             for title in titles:
-                group = Card_Groups(card_package = cardPackage, title = title)
+                group = Card_Groups(card_package=cardPackage, title=title)
                 group.save()
             for text in texts:
-                card = Cards(card_package = cardPackage, card_group = group, text = text)
+                card = Cards(card_package=cardPackage, card_group=group, text=text)
                 card.save()
             return render(
-		    request,
-            'index.html',
+                request,
+                'index.html',
             )
         else:
             form = CreateCardPackage(instance=Card_Packages())
             args = {'form': form}
             return render(
-            request,
-            'project.html',
-            {'form': form}
-        )
+                request,
+                'project.html',
+                {'form': form}
+            )
     else:
         form = CreateCardPackage(instance=Card_Packages())
         args = {'form': form}
         return render(
-        request,
-        'project.html',
-		{'form': form}
-    )
-	
+            request,
+            'project.html',
+            {'form': form}
+        )
+
+
 def register(request):
-    if request.method =='POST':
+    if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
@@ -79,58 +84,64 @@ def register(request):
             user = authenticate(username=username, password=password)
             login(request, user)
             return render(
-            request,
-            'index.html',
+                request,
+                'index.html',
             )
         else:
             form = RegistrationForm()
             args = {'form': form}
             return render(
-            request,
-            'register.html',
-            {'form': form}
-        )
+                request,
+                'register.html',
+                {'form': form}
+            )
     else:
         form = RegistrationForm()
         args = {'form': form}
         return render(
-        request,
-        'register.html',
-		{'form': form}
-    )
-	
+            request,
+            'register.html',
+            {'form': form}
+        )
+
+
 def view(request):
-	cardPackages = Card_Packages.objects.all()
-	return render(
-		request,
-		'view.html',
+    cardPackages = Card_Packages.objects.all()
+    return render(
+        request,
+        'view.html',
     )
 
+
 def package(request):
-	cardPackages = Card_Packages.objects.all()
-	context = {'cardPackages': cardPackages}
-	return render(
-		request,
-		'package.html',
-		context
+    cardPackages = Card_Packages.objects.all()
+    context = {'cardPackages': cardPackages}
+    return render(
+        request,
+        'package.html',
+        context
     )
-	
-def packageList(request, packagePK):
-	package = Card_Packages.objects.get(id__exact=packagePK)
-	context = {'package': package}
-	return render(
-		request,
-		'packageList.html',
-		context
-    )	
-	
+
+
+def packageList(request, packagepk):
+    package = Card_Packages.objects.get(id__exact=packagepk)
+    context = {'package': package}
+    return render(
+        request,
+        'packageList.html',
+        context
+    )
+
+
 def cardPackages(request):
     cardPackages = Card_Packages.objects.all() 
     return TemplateResponse(request, views.index, {'cardPackages': cardPackages})
-	
+
+
 def cardGroups(request):
-    cardGroups = Card_Groups.objects.all() 
+    cardGroups = Card_Groups.objects.all()
     return TemplateResponse(request, views.index, {'cardGroups': cardGroups})
+
 
 def cardList(request):
     cardList = Cards.objects.all() 
