2499ccda1c763f0070399b8ef76db5e281ba477a
bendalll <42238838+bendalll@users.noreply.github.com>
2018-08-21 16:36:51 +1000

    Removed migrations to re-run from scratc

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'MyDigitalHealth.apps.MydigitalhealthConfig', 
+    'MyDigitalHealth.apps.MydigitalhealthConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,8 +55,8 @@ ROOT_URLCONF = 'MyProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['./templates',
-				 './templates/registration'],
+        'DIRS': ['./MyDigitalHealth/templates',
+                 './templates/registration'],  # TODO resolve this
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -65,8 +65,8 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'MyDigitalHealth.context_processors.cardPackages',
-				'MyDigitalHealth.context_processors.cardGroups',
-				'MyDigitalHealth.context_processors.cardList',
+                'MyDigitalHealth.context_processors.cardGroups',
+                'MyDigitalHealth.context_processors.cardList',
             ],
         },
     },
