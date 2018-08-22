f88cd8e14a882f16f70dffa9f2f32bce185bc4e9
Daedalusz <daedalus.z@gmail.com>
2018-08-22 18:02:36 +1000

    Updated Login/Logout for Django 2.1
    Updated Login/Logout Class syntax for Django 2.1 changes.

--- MyDigitalHealth/urls.py
+++ MyDigitalHealth/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, reverse
 from django.contrib import admin
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', login, {'template_name': 'login.html'}, name='login'),
-	path('logout', logout, name='logout'),
+    path('login', LoginView, {'template_name': 'login.html'}, name='login'),
+	path('logout', LogoutView, name='logout'),
 	path('create', views.create, name='create'),
 	path('register', views.register, name='register'),
 	path('cards', views.cards, name='cards'),
