3424311e263b4c87477531b53933ae3f18c77dba
bendalll <42238838+bendalll@users.noreply.github.com>
2018-08-23 10:54:22 +1000

    Update urls to work with Django 2.1

--- MyDigitalHealth/urls.py
+++ MyDigitalHealth/urls.py
@@ -1,20 +1,19 @@
-from django.urls import path, reverse
-from django.contrib import admin
-from django.contrib.auth.views import login, logout
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', login, {'template_name': 'login.html'}, name='login'),
-	path('logout', logout, name='logout'),
-	path('create', views.create, name='create'),
-	path('register', views.register, name='register'),
-	path('cards', views.cards, name='cards'),
-	path('view', views.view, name='view'),
-	path('package', views.package, name='package'),
-	path('packageList/<package>/', views.packageList, name='packageList'),
-	path('comments', views.comments, name='comments'),
-	path('admin', views.admin, name='admin'),
-	path('edit/<package>/', views.edit, name='edit'),
-	path('editPackage/<package>/', views.editPackage, name='editPackage'),
+    path('login', LoginView, {'template_name': 'login.html'}, name='login'),
+    path('logout', LogoutView, name='logout'),
+    path('create', views.create, name='create'),
+    path('register', views.register, name='register'),
+    path('cards', views.cards, name='cards'),
+    path('view', views.view, name='view'),
+    path('package', views.package, name='package'),
+    path('packageList/<package_id>/', views.packageList, name='packageList'),
+    path('comments', views.comments, name='comments'),
+    path('admin', views.admin, name='admin'),
+    path('edit/<package>/', views.edit, name='edit'),
+    path('editPackage/<package_id>/', views.editPackage, name='editPackage'),
 ]
