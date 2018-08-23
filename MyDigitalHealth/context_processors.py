a1fc24fad0146ed034614579947e50d54d2c84b3
bendalll <42238838+bendalll@users.noreply.github.com>
2018-08-23 23:15:18 +1000

    Updating html and views functionality
    Mainly for Package preview

--- MyDigitalHealth/context_processors.py
+++ MyDigitalHealth/context_processors.py
@@ -3,17 +3,17 @@ from .models import Package, Category, Card
 
 def card_packages(request):
     return {
-        'cardPackages': Package.objects.all()
+        'all_packages': Package.objects.all()
     }
 
 
 def card_categories(request):
     return {
-        'cardGroups': Category.objects.all()
+        'card_categories': Category.objects.all()
     }
 
 
 def all_cards(request):
     return {
-        'cardList': Card.objects.all()
+        'card_list': Card.objects.all()
     }
