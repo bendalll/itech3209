157cc2cbf8aab5d2fa74eaf63eb84e80bd4a4be9
bendalll <42238838+bendalll@users.noreply.github.com>
2018-08-23 23:15:39 +1000

    Updating package preview functionality

--- MyDigitalHealth/views.py
+++ MyDigitalHealth/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import login, authenticate
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegistrationForm, CreateCardPackage, CreateCardGroup, CreateCards, CreateComments
 from .models import Package, Category, Card, Comment
 from django.template.response import TemplateResponse
@@ -21,6 +21,43 @@ def index(request):
     )
 
 
+def register(request):
+    """
+    View to create a new user with data provided
+    **Fails if the password does not meet the requirements, without advising the user of the reason**
+    If successful, creates the user, logs them in and sends to home page;
+    If fails, refreshes the page with no additional information currently
+    """
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return render(
+                request,
+                'index.html',
+            )
+        else:  # TODO meaningful "else" functionality
+            form = RegistrationForm()
+            args = {'form': form}
+            return render(
+                request,
+                'register.html',
+                {'form': form}
+            )
+    else:  # TODO meaningful "else" functionality
+        form = RegistrationForm()
+        args = {'form': form}
+        return render(
+            request,
+            'register.html',
+            {'form': form}
+        )
+
+
 def create_package(request):
     """
     Administrator functionality to create new Packages with related Cards and Categories
@@ -36,6 +73,8 @@ def create_package(request):
         if request.method == 'POST':
             # get data from request.POST and create cards and categories and new package as objects
             # save new objects to the database
+            # TODO: this should really return an "active package" object with the active items - card text and cat names
+            # etc in a dict so they can be easily accessed via template code. Somewhere should define "create pack obj"
             data = request.POST
             new_package = Package(package_name=data['package_name'], owner=request.user)
             new_package.save()
@@ -49,80 +88,29 @@ def create_package(request):
                 new_card.save()
 
             # load the admin-view-all-the-packages page
-            return render(
-                request,
-                'admin.html',
-            )
-# TODO else throw 404?
+            return redirect('admin')
+# TODO else do something useful
 
 
-def preview_package(request):
+def package_preview(request, package_id):
+    active_package = Package.get_package_by_id(package_id)
+    # TODO: this should be a "collected package" object with all the card text / cat names etc in one object
+    # to pass through as context
+    context = {'active_package': active_package,
+               'card_text': ["Message from the past", "Message from the future"],
+               'category_name': ["Move it here", "No, move it here"]
+               }
     return render(
         request,
         'package_preview.html',
-    )
-
-
-def register(request):
-    if request.method == 'POST':
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            return render(
-                request,
-                'index.html',
-            )
-        else:
-            form = RegistrationForm()
-            args = {'form': form}
-            return render(
-                request,
-                'register.html',
-                {'form': form}
-            )
-    else:
-        form = RegistrationForm()
-        args = {'form': form}
-        return render(
-            request,
-            'register.html',
-            {'form': form}
-        )
-
-
-def view(request):
-    cardPackages = Package.objects.all()
-    return render(
-        request,
-        'view.html',
-    )
-
-
-def package(request):
-    card_packages = Package.objects.all()
-    context = {'cardPackages': cardPackages}
-    return render(
-        request,
-        'packages_list.html',
-        context
-    )
-
-
-def package_list(request, package_id):
-    package = Package.objects.get(id__exact=package_id)
-    context = {'package': package}
-    return render(
-        request,
-        'package_active.html',
         context
     )
 
 
 def admin(request):
+    """
+    View to generate and display the Administration page with a list of the logged-in user's packages that they own
+    """
     owner = request.user
     own_packages = Package.get_packages_by_owner(owner)
     context = {'own_packages': own_packages}
@@ -133,18 +121,41 @@ def admin(request):
     )
 
 
-def edit(request, package):
-    package = Package.objects.get(id__exact=package)
-    context = {'package': package}
+def view(request):
+    all_packages = Package.objects.all()
+    context = {'all_packages': all_packages}
     return render(
         request,
-        'edit.html',
+        'view.html',
+        context,
+    )
+
+
+def package(request):
+    all_packages = Package.objects.all()
+    context = {'all_packages': all_packages}
+    return render(
+        request,
+        'packages_dropdown_list.html',
+        context
+    )
+
+
+def open_package(request, package_id):
+    """
+    Take a package id and generate the activity page for the user to card sort with that package
+    """
+    active_package = Package.objects.get(id__exact=package_id)
+    context = {'active_package': active_package}
+    return render(
+        request,
+        'package_active.html',
         context
     )
 
 
 def comments(request):
-    if request.method =='POST':
+    if request.method == 'POST':
         form = CreateComments(request.POST, instance=Comment())
         name = request.POST.get('name')
         cardPackage = Package.objects.get(name = name)
@@ -164,6 +175,16 @@ def comments(request):
             'package_active.html',
             {'form': form}
         )
+
+
+def edit(request, package_id):
+    active_package = Package.objects.filter(package_id=package_id)
+    context = {'active_package': active_package}
+    return render(
+        request,
+        'edit.html',
+        context
+    )
 
 
 def editPackage(request, package):
@@ -214,16 +235,16 @@ def editPackage(request, package):
         )
 
 
-def cardPackages(request):
-    cardPackages = Package.objects.all()
-    return TemplateResponse(request, views.index, {'cardPackages': cardPackages})
+# def cardPackages(request):
+#     cardPackages = Package.objects.all()
+#     return TemplateResponse(request, views.index, {'cardPackages': cardPackages})
 
 
-def cardGroups(request):
-    cardGroups = Category.objects.all()
-    return TemplateResponse(request, views.index, {'cardGroups': cardGroups})
+# def cardGroups(request):
+#     cardGroups = Category.objects.all()
+#     return TemplateResponse(request, views.index, {'cardGroups': cardGroups})
 
 
-def cardList(request):
-    cardList = Card.objects.all()
-    return TemplateResponse(request, views.index, {'cardList': cardList})
+# def cardList(request):
+#     cardList = Card.objects.all()
+#     return TemplateResponse(request, views.index, {'cardList': cardList})
