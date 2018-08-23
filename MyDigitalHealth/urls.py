6500fe408e04f60a17312fb7a1d7f35cb4144fb6
bendalll <42238838+bendalll@users.noreply.github.com>
2018-08-23 11:40:03 +1000

    Update urls to work with Django 2.1

--- MyDigitalHealth/urls.py
+++ MyDigitalHealth/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', login, {'template_name': 'login.html'}, name='login'),
-    path('logout', logout, name='logout'),
+    path('login', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout', LogoutView.as_view(template_name='index.html'), name='logout'),
     path('create', views.create, name='create'),
     path('register', views.register, name='register'),
     path('cards', views.cards, name='cards'),
@@ -15,5 +15,5 @@ urlpatterns = [
     path('comments', views.comments, name='comments'),
     path('admin', views.admin, name='admin'),
     path('edit/<package>/', views.edit, name='edit'),
-    path('editPackage/<package>/', views.editPackage, name='editPackage'),
+    path('editPackage/<package_id>/', views.editPackage, name='editPackage'),
 ]
