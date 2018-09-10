f26ac2b5cdda48d4eb60b888ee9215f19f7578a1
30305269 <42342516+30305269@users.noreply.github.com>
2018-09-10 21:51:25 +1000

    Add files via upload

--- MyDigitalHealth/static/scripts/view.js
+++ MyDigitalHealth/static/scripts/view.js
@@ -16,11 +16,65 @@
 	
 	$( function() 
 	{
-		$( ".connection" ).sortable(
+		$(".connection").sortable(
 			{
 				connectWith: ".connection",
+				update: function(event, ui)
+				{
+					var result = $(this).sortable('toArray', {attribute: 'value'});
+					var test = $(".connection").sortable('toArray', {attribute: 'value'});
+					//List Cards from Card Package into Array.
+					var cardsText = [];
+					$("#sortable li").each(function()
+					{ 
+						cardsText.push($(this).text()) 
+					});
+					//var cardString = cardsText.join(' ');
+
+					document.getElementById("cardsList").value = cardsText;
+					
+					
+					var length = $(".cardGroups").length
+
+					
+					var numberOfGroups = $(".cardGroups");
+					
+					
+					$(numberOfGroups).each(function(i)
+					{
+						var optionTexts = [];
+						optionTexts.push($(this).text()) 
+						var cardString = optionTexts.map(str => str.trim());
+						//console.log(Array.isArray(optionTexts));
+						//document.getElementById("pe" + (i + 1)).innerHTML = cardString;
+						document.getElementById('sortedCards' + (i + 1)).value = cardString;
+					});
+				}
 			});
 
+	});
+	
+	$( function() 
+	{
+		$(document).ready(function() 
+		{
+			$(".cardGroups").each(function(i) 
+			{
+				$(this).attr('id', "cardGroups" + (i + 1));
+						
+				//delete
+				//var createInputHeading = document.createElement('p');
+				//$(createInputHeading).attr("id", "pe" + (i + 1));
+				//$("#forming").append(createInputHeading);
+
+				var sortedCards = document.createElement('input');
+				$(sortedCards).attr("id", "sortedCards" + (i + 1));
+				$(sortedCards).attr("name", "sortedCards");
+				$(sortedCards).attr("type","hidden");
+				$("#submitForm").append(sortedCards);
+				
+			})
+		})
 	});
 	
 	
