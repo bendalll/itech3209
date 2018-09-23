dbcf3eac74a78eeeb80bc67a38927949e8f20ee7
bendalll <42238838+bendalll@users.noreply.github.com>
2018-09-23 14:41:54 +1000

    Revert "Merge branch 'master' of git://github.com/30305269/itech3209 into 30305269-master"
    This reverts commit 6b26289fea261425f0236ad9fdb56ea47a93696b, reversing
    changes made to 6df974a0abe3923e0a3fb52ad83359068ce0aa6f.

--- MyDigitalHealth/static/scripts/view.js
+++ MyDigitalHealth/static/scripts/view.js
@@ -16,105 +16,11 @@
 	
 	$( function() 
 	{
-<<<<<<< HEAD
 		$( ".connection" ).sortable(
 			{
 				connectWith: ".connection",
-=======
-		$(".connection").sortable(
-			{
-				connectWith: ".connection",
-				update: function(event, ui)
-				{
-					//List Cards from Card Package into Array.
-					var cardsText = [];
-					$("#sortable li").each(function()
-					{ 
-						cardsText.push($(this).val()) 
-					});
-					//var cardString = cardsText.join(' ');
-
-					document.getElementById("cardsList").value = cardsText;
-					
-					$(".cardGroups").each(function(i)
-					{
-						var test = $(this).val()
-						var sortedCard = [];
-						
-						$(this).find("li").each(function()
-						{ 
-							sortedCard.push($(this).val()) 
-						});
-						document.getElementById('sortedCards' + (i + 1)).value = [sortedCard];
-					});
-					
-					$(".sortedCardGroups").each(function(i)
-					{
-						var test = $(this).val()
-						var sortedCardList = [];
-						
-						$(this).find("li").each(function()
-						{ 
-							sortedCardList.push($(this).val()) 
-						});
-						document.getElementById('sortedCardList' + (i + 1)).value = [sortedCardList];
-					});
-				}
 			});
 
-	});
-	
-	$( function() 
-	{
-		$(document).ready(function() 
-		{
-			$("#cardGroupsDiv .cardGroups").each(function(i) 
-			{
-				$(this).attr('id', "cardGroups" + (i + 1));
-						
-				//delete
-				//var createInputHeading = document.createElement('p');
-				//$(createInputHeading).attr("id", "pe" + (i + 1));
-				//$("#forming").append(createInputHeading);
-
-				var sortedCards = document.createElement('input');
-				$(sortedCards).attr("id", "sortedCards" + (i + 1));
-				$(sortedCards).attr("name", "sortedCards");
-				$(sortedCards).attr("type","hidden");
-				$(submitForm).append(sortedCards);
-				
-			})
-
-			$(".sortedCardGroups").each(function(i) 
-			{
-				$(this).attr('id', "sortedCardGroups" + (i + 1));
-						
-				//delete
-				//var createInputHeading = document.createElement('p');
-				//$(createInputHeading).attr("id", "pe" + (i + 1));
-				//$("#forming").append(createInputHeading);
-
-				var sortedCardList = document.createElement('input');
-				$(sortedCardList).attr("id", "sortedCardList" + (i + 1));
-				$(sortedCardList).attr("name", "sortedCardList");
-				$(sortedCardList).attr("type","hidden");
-				$(submitForm).append(sortedCardList);
-				
-			})
-			
-			var sortedCardGroup = $('#cardGroupsDiv');
-			var sortedCardGroupsList = sortedCardGroup.children('.cards').get();
-			sortedCardGroupsList.sort(function(a, b) 
-			{
-				return $(a).text().toUpperCase().localeCompare($(b).text().toUpperCase());
->>>>>>> ff00133193e3c787ae015e55abe615962e6a04b4
-			});
-
-			$.each(sortedCardGroupsList, function(index, cardGroup) 
-			{
-				sortedCardGroup.append(cardGroup); 
-			});
-		})
 	});
 	
 	
