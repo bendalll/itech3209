50f60c1cf88aae1309643b75148cfcc4198b4578
bendalll <42238838+bendalll@users.noreply.github.com>
2018-10-14 21:07:55 +1100

    Remove unused context processors

--- MyDigitalHealth/context_processors.py
+++ MyDigitalHealth/context_processors.py
@@ -1,25 +1,5 @@
-from .models import Card_Packages, Card_Groups, Cards, Comments, Sorted_Package
+from .models import Card_Packages
+
 
 def cardPackages(request):
-    return {
-        'cardPackages': Card_Packages.objects.all()
-    }
-def cardGroups(request):
-    return {
-        'cardGroups': Card_Groups.objects.all()
-    }
-	
-def cardList(request):
-    return {
-        'cardList': Cards.objects.all()
-    }
-
-def commentList(request):
-    return {
-        'commentList': Comments.objects.all()
-    }	
-	
-def sortedList(request):
-    return {
-        'sortedList': Sorted_Package.objects.all()
-    }		
+    return {'cardPackages': Card_Packages.objects.all()}
