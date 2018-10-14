176cc0167984fb1e0b22f64a58aa52af7e23f3d3
bendalll <42238838+bendalll@users.noreply.github.com>
2018-10-14 21:07:17 +1100

    Remove Comments to refactor Sorted_Package, add Permissions

--- MyDigitalHealth/models.py
+++ MyDigitalHealth/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Card_Packages(models.Model):
     name = models.CharField(max_length=200)
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    owner = models.ForeignKey(User, on_delete=models.PROTECT)
     main_color = models.CharField(max_length=7, default='#337ab7')
     comments_allowed = models.BooleanField(default=True)
     user_defined_groups = models.BooleanField(default=False)
@@ -22,6 +22,32 @@ class Card_Packages(models.Model):
     def get_cards(self):
         cards = Cards.objects.filter(card_package=self)
         return cards
+
+    def to_dict(self):
+        """ Used to return a package object as a dict, with package name, list of categories, and list of cards """
+        list_of_group_objects = Card_Groups.objects.filter(card_package=self)
+        list_of_card_objects = Cards.objects.filter(card_package=self)
+
+        package = {'package_id': self.pk,
+                   'name': self.name,
+                   'main_color': self.main_color,
+                   'comments_allowed': self.comments_allowed,
+                   'card_groups': list_of_group_objects,
+                   'cards': list_of_card_objects,
+                   }
+        return package
+
+
+class Sorted_Packages(Card_Packages):
+    parent_package = models.ForeignKey(Card_Packages, on_delete=models.CASCADE, related_name='base_package')
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    comment = models.CharField(max_length=200, default='Placeholder')
+
+    def __str__(self):
+        return "Sorted" + self.name + " for " + self.user.username
+
+    class Meta:
+        verbose_name_plural = 'Sorted_Packages'
 
 
 class Card_Groups(models.Model):
@@ -47,24 +73,7 @@ class Cards(models.Model):
         verbose_name_plural = 'Cards'
 
 
-class Comments(models.Model):
+class Permissions(models.Model):
+    """ User Access Control for package assignation """
     card_package = models.ForeignKey(Card_Packages, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
-    comment = models.CharField(max_length=200, default='Placeholder')
-
-    def __str__(self):
-        return self.comment
-
-    class Meta:
-        verbose_name_plural = 'Comments'
-
-
-class Sorted_Package(models.Model):
-    card_package = models.ForeignKey(Card_Packages, on_delete=models.CASCADE)
-    card_group = models.ForeignKey(Card_Groups, on_delete=models.CASCADE, blank=True, null=True)
-    user_titles = models.CharField(max_length=200, blank=True, null=True)
-    cards = models.ManyToManyField(Cards)
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
-
-    class Meta:
-        verbose_name_plural = 'Sorted_Packages'
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
