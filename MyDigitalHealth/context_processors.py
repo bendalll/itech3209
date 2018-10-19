6a4a0e9ff02a1dec17e269bdb10dbe53c787c2fe
Daedalusz <daedalus.z@gmail.com>
2018-10-20 01:05:16 +1100

    Updated to errors with unauthenticated users.
    Updated to check if user is authenticated before attempting to return a list of packages to prevent system errors when anoymous users attempt to access the site.
    
    Also hid Card Package drop down menu entirely from unauthenticated users. Mostly for aesthetics.

--- MyDigitalHealth/context_processors.py
+++ MyDigitalHealth/context_processors.py
@@ -3,13 +3,17 @@ from .models import Permission, Package
 
 def card_packages(request):
     user = request.user
-    packages = []
-    permissions = Permission.objects.filter(user=user)
-    for permission in permissions:
-        packages.append(permission.package)
 
-    if user.is_staff:
-        own_packages = Package.objects.filter(owner=user)
-        for package in own_packages:
-            packages.append(package)
-    return {'card_packages': packages}
+    if user.is_authenticated:
+        packages = []
+        permissions = Permission.objects.filter(user=user)
+        for permission in permissions:
+            packages.append(permission.package)
+
+        if user.is_staff:
+            own_packages = Package.objects.filter(owner=user)
+            for package in own_packages:
+                packages.append(package)
+        return {'card_packages': packages}
+    return {'': ''}
+
